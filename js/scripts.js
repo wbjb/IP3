0d284d9e140f5ab8ec1e75268cd75b26e82c3027
cmdr33 <driveguy44@gmail.com>
2018-12-10 05:47:39 -0500

    Added some scripts to interact with the website.

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+function submitAnswers(){
+	var total = 5;
+	var score = 0;
+
+
+	var q1 = document.forms["questionForm"]["q1"].value;
+	var q2 = document.forms["questionForm"]["q2"].value;
+	var q3 = document.forms["questionForm"]["q3"].value;
+	var q4 = document.forms["questionForm"]["q4"].value;
+	var q5 = document.forms["questionForm"]["q5"].value;
+	var q6 = document.forms["questionForm"]["q6"].value;
+	var q7 = document.forms["questionForm"]["q7"].value;
+	var q8 = document.forms["questionForm"]["q8"].value;
+	var q9 = document.forms["questionForm"]["q9"].value;
+	var q10 = document.forms["questionForm"]["q10"].value;
+
+
+
+	var answers = ["c","d","d","d","c","a","a","d","b","b"]
+	for (i = 1; i <= total;i++){
+		if(eval('q'+i) == answers[i-1]){
+			score++;
+		}
+	}
+
+	var results = document.getElementById('results');
+	results.innerHTML = 'Score:'+score/total*100;
+	alert('You got '+score/total*100+'%');
+
+	return false;
+}
